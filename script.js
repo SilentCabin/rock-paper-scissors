c9d5216d94a4cf32c24ba56926f7fac54e118635
Silent <75711387+SilentCabin@users.noreply.github.com>
2024-08-20 01:40:16 +0100

    <feat> create humanChoice function + score vars

--- script.js
+++ script.js
@@ -2,5 +2,17 @@ function getComputerChoice(){
     let choices = ["Rock", "Paper", "Scissors"]
     let computerChoice = choices[Math.floor(Math.random() * choices.length)];
     console.log(computerChoice);
+    return computerChoice;
 }
+
+function getHumanChoice() {
+    let humanChoice = prompt("Enter Rock, Paper or Scissors:");
+    console.log(humanChoice);
+    
+}
+
+let humanScore = 0;
+let computerScore = 0;
 getComputerChoice();
+getHumanChoice();
+
