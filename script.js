efc075e882fdcf3653020db0f5c8f2e37222a25a
Silent <75711387+SilentCabin@users.noreply.github.com>
2024-09-03 02:01:55 +0100

    <feat> add playRound function and tidies up code

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function getComputerChoice(){
+  //array with choices
     let choices = ["Rock", "Paper", "Scissors"]
+  //select random item from array
     let computerChoice = choices[Math.floor(Math.random() * choices.length)];
     console.log(computerChoice);
     return computerChoice;
@@ -7,13 +9,35 @@ function getComputerChoice(){
 
 function getHumanChoice() {
   // human input
+    let humanChoice = prompt("Enter Rock, Paper or Scissors:");
     console.log(humanChoice);
+    return humanChoice;
     
+}
+
+function playRound(humanChoice, computerChoice) {
+  //convert to lowercase to compare strings
+  humanChoice = humanChoice.toLowerCase();
+  computerChoice = computerChoice.toLowerCase();
+  //check to see if tie
+  if (humanChoice === computerChoice) {
+    console.log (`DRAW! ${humanChoice} ties to ${computerChoice}`)
+  //check win options
+  } else if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper") 
+  ) {
+    console.log (`YOU WIN!!! ${humanChoice} beats ${computerChoice}`)
+  } else {
+    console.log(`YOU LOSE!!! ${computerChoice} beats ${humanChoice}`);
+    
+  }
 }
 
 let humanScore = 0;
 let computerScore = 0;
-getComputerChoice();
-getHumanChoice();
+let computerChoice = getComputerChoice();
+let humanChoice = getHumanChoice();
 
-//let humanChoice = prompt("Enter Rock, Paper or Scissors:");
+playRound(humanChoice, computerChoice);
